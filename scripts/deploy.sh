9e9bbddd8761debf2e621aa7ae1913ce98d8e7e8
Sunil Sattiraju <sunilsattiraju@gmail.com>
2023-04-15 12:20:27 +0800

    deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,17 @@
 output=$(azd env get-values)
 
 while IFS= read -r line; do
-    name=$(echo "$line" | cut -d'=' -f1)
-    value=$(echo "$line" | cut -d'=' -f2- | sed 's/^"\|"$//g')
-    export "$name"="$value"
-done <<< "$output"
+  name=$(echo "$line" | cut -d'=' -f1)
+  value=$(echo "$line" | cut -d'=' -f2- | sed 's/^"\|"$//g')
+  export "$name"="$value"
+done <<<"$output"
 
 echo "Environment variables set."
 
 commands=("az" "swa" "func")
 
 for cmd in "${commands[@]}"; do
-  if ! command -v "$cmd" &> /dev/null; then
+  if ! command -v "$cmd" &>/dev/null; then
     echo "Error: $cmd command is not available, check pre-requisites in README.md"
     exit 1
   fi
@@ -23,7 +23,11 @@ az account set --subscription $AZURE_SUBSCRIPTION_ID
 
 cd ./app/frontend
 SWA_DEPLOYMENT_TOKEN=$(az staticwebapp secrets list --name $AZURE_STATICWEBSITE_NAME --query "properties.apiKey" --output tsv)
-swa deploy --env production --deployment-token $SWA_DEPLOYMENT_TOKEN
+if [[ -n $SWA_DEPLOYMENT_TOKEN ]]; then
+  swa deploy --env production --deployment-token $SWA_DEPLOYMENT_TOKEN
+else
+  echo "SWA_DEPLOYMENT_TOKEN is empty, not deployoing froentend, check if the static website is created in Azure portal."
+fi
 
 cd ../backend
 # AZURE_FORM_RECOGNIZER_KEY=$(az cognitiveservices account keys list --name $AZURE_FORMRECOGNIZER_SERVICE --resource-group $AZURE_RESOURCE_GROUP --query "key1" --output tsv)
@@ -32,10 +36,13 @@ AZURE_SEARCH_KEY=$(az cognitiveservices account keys list --name $AZURE_SEARCH_S
 if [[ -n $AZURE_SEARCH_KEY ]]; then
   az functionapp config appsettings list --name $AZURE_FUNCTION_NAME --resource-group $AZURE_RESOURCE_GROUP --settings AZURE_SEARCH_KEY=$AZURE_SEARCH_KEY
 else
-  echo "AZURE_SEARCH_KEY is empty, not setting it. go to Azure portal and set it manually."
+  echo "AZURE_SEARCH_KEY is empty, not setting it. Go to Azure portal and set it manually."
 fi
 
-func azure functionapp publish $AZURE_FUNCTION_NAME
+func azure functionapp publish $AZURE_FUNCTION_NAME --python
+
+echo "Deployed successfully."
+cd ../..
 
 # Purge a deleted resource
 # az resource delete --ids /subscriptions/$AZURE_SUBSCRIPTION_ID/resourceGroups/$AZURE_RESOURCE_GROUP/providers/Microsoft.CognitiveServices/accounts/$AZURE_FORMRECOGNIZER_SERVICE
@@ -43,5 +50,3 @@ func azure functionapp publish $AZURE_FUNCTION_NAME
 
 # recover
 # az cognitiveservices account recover --location $AZURE_LOCATION --name $AZURE_FORMRECOGNIZER_SERVICE --resource-group $AZURE_RESOURCE_GROUP
-# az cognitiveservices account recover --location eastus --name cog-y2nmeuebipp4i --resource-group $AZURE_RESOURCE_GROUP
-# az cognitiveservices account delete --location $AZURE_LOCATION --name cog-y2nmeuebipp4i --resource-group $AZURE_RESOURCE_GROUP
