d37d04dce33c579e4559f83946de957317174b67
Sunil Sattiraju <sunilsattiraju@gmail.com>
2023-04-19 20:04:08 +0800

    update langchain to latest and callback handler

--- app/backend/FlaskApp/langchainadapters.py
+++ app/backend/FlaskApp/langchainadapters.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish, LLMResult
 
@@ -6,26 +6,34 @@ def ch(text: str) -> str:
     s = text if isinstance(text, str) else str(text)
     return s.replace("<", "&lt;").replace(">", "&gt;").replace("\r", "").replace("\n", "<br>")
 
-class HtmlCallbackHandler (BaseCallbackHandler):
+class HtmlCallbackHandler(BaseCallbackHandler):
     html: str = ""
 
     def get_and_reset_log(self) -> str:
         result = self.html
         self.html = ""
         return result
-    
+
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
         """Print out the prompts."""
-        self.html += f"LLM prompts:<br>" + "<br>".join(ch(prompts)) + "<br>";
+        self.html += f"LLM prompts:<br>" + \
+            "<br>".join(ch(prompts)) + "<br>"  # type: ignore
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Do nothing."""
         pass
 
-    def on_llm_error(self, error: Exception, **kwargs: Any) -> None:
-        self.html += f"<span style='color:red'>LLM error: {ch(error)}</span><br>"
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Do nothing."""
+        pass
+
+    def on_llm_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        # type: ignore
+        self.html += f"<span style='color:red'>LLM error: {ch(error)}</span><br>" # type: ignore
 
     def on_chain_start(
         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
@@ -33,23 +41,34 @@ class HtmlCallbackHandler (BaseCallbackHandler):
         """Print out that we are entering a chain."""
         class_name = serialized["name"]
         self.html += f"Entering chain: {ch(class_name)}<br>"
+        # logging.info(f"\n\n\033[1m> Entering new {class_name} chain...\033[0m")
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Print out that we finished a chain."""
         self.html += f"Finished chain<br>"
+        # logging.info("\n\033[1m> Finished chain.\033[0m")
 
-    def on_chain_error(self, error: Exception, **kwargs: Any) -> None:
-        self.html += f"<span style='color:red'>Chain error: {ch(error)}</span><br>"
+    def on_chain_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        # type: ignore
+        self.html += f"<span style='color:red'>Chain error: {ch(error)}</span><br>" # type: ignore
 
     def on_tool_start(
         self,
         serialized: Dict[str, Any],
-        action: AgentAction,
-        color: Optional[str] = None,
+        input_str: str,
         **kwargs: Any,
     ) -> None:
-        """Print out the log in specified color."""
+        self.html += f"<span>{ch(input_str)}</span><br>"
+
+
+    def on_agent_action(
+        self, action: AgentAction, color: Optional[str] = None, **kwargs: Any
+    ) -> Any:
+        """Run on agent action."""
         self.html += f"<span style='color:{color}'>{ch(action.log)}</span><br>"
+        # logging.info(action)
 
     def on_tool_end(
         self,
@@ -59,11 +78,16 @@ class HtmlCallbackHandler (BaseCallbackHandler):
         llm_prefix: Optional[str] = None,
         **kwargs: Any,
     ) -> None:
+        # type: ignore
+        self.html += f"{ch(observation_prefix)}<br>{ch(output)}<br>{ch(llm_prefix)}<br>" # type: ignore
         """If not the final action, print out observation."""
-        self.html += f"{ch(observation_prefix)}<br><span style='color:{color}'>{ch(output)}</span><br>{ch(llm_prefix)}<br>"
+        # logging.info(output)
 
-    def on_tool_error(self, error: Exception, **kwargs: Any) -> None:
-        self.html += f"<span style='color:red'>Tool error: {ch(error)}</span><br>"
+    def on_tool_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        # type: ignore
+        self.html += f"<span style='color:red'>Tool error: {ch(error)}</span><br>" # type: ignore
 
     def on_text(
         self,
@@ -74,9 +98,11 @@ class HtmlCallbackHandler (BaseCallbackHandler):
     ) -> None:
         """Run when agent ends."""
         self.html += f"<span style='color:{color}'>{ch(text)}</span><br>"
+        # logging.info(text)
 
     def on_agent_finish(
         self, finish: AgentFinish, color: Optional[str] = None, **kwargs: Any
     ) -> None:
         """Run on agent end."""
         self.html += f"<span style='color:{color}'>{ch(finish.log)}</span><br>"
+        # logging.info(finish.log)
